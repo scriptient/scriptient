aea47f44de4fa66d60ea2717b62daa4297ba1947
Armando Vasquez <ar.xa.vasquez@gmail.com>
2018-10-18 17:17:16 -0500

    Very basic initial commit
    Only features the default content, a mongodb connection (local only), and a very rudimentary admins.js page that implements the mongodb data

---
+++ src/pages/admins.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { graphql } from "gatsby"
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+class AdminList extends React.Component {
+
+    constructor(data) {
+
+        super();
+        this.state = {
+            admins: data.data.edges
+        }
+
+    }
+
+    render() {
+        
+        const listItems = this.state.admins.map((admin) =>
+            <li key={admin.node.first_name}>     
+                {admin.node.first_name}
+            </li>
+        );
+
+        return (
+            <ul>{listItems}</ul>
+        )
+
+    }
+
+}
+
+export default ({ data }) => {
+
+    return (
+        <Layout>
+            console.log(data)
+            <h1>Web App Administrators</h1>
+            <AdminList data = {data.allMongodbScriptientDbAdminUsers} />
+            <Link to="/">Home</Link>
+        </Layout>
+    )
+}
+
+export const query = graphql`
+query {
+    allMongodbScriptientDbAdminUsers {
+            edges {
+                node {
+                    mongodb_id
+                    first_name
+                    last_name
+                    email
+                }
+            }
+    }
+}
+`
